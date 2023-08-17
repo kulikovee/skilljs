11bd11711a0e31310a6b87500e9075f617306305
vitality <qwerty@qwerty.com>
2023-08-17 20:03:06 +0300

    #12. Client: add webpack react and ts

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import React, {FC} from 'react';
+
+const App: FC = () => {
+    return (
+        <div>
+            testdawddwdwADW
+        </div>
+    );
+};
+
+export default App;
